4288220830ed85cafc2f108d41f71d8af4dc0f71
Wilberto Duran <Willfontalvodev@gmail.com>
2023-07-07 18:04:16 -0500

    convertir imagenes a webp por medio de gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 
-const {src, dest, watch} = require("gulp");
+const {src, dest, watch, parallel} = require("gulp");
+
+//CSS
 const sass = require("gulp-sass") (require("sass"));
 const plumber = require('gulp-plumber');
 
+//imagenes
+const webp= require('gulp-webp');
+
 function css(done){
-
-
     src('src/SCSS/**/*.scss')    //identificar el archivo de SASS
     .pipe(plumber()) //para que no se detenga el watch en la terminal
     .pipe(sass())    //compilarlo
@@ -13,6 +16,18 @@ function css(done){
     done() // callback que avisa a gulp cuando llegamos al final
 }
 
+
+function versionWebp(done){
+    const opciones = {
+        quality: 50
+    }
+
+    src('src/img/**/*.{jpg,png}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+ 
+    done()
+}
 function dev(done){
     watch("src/SCSS/**/*.scss", css);
 
@@ -20,4 +35,5 @@ function dev(done){
 }
 
 exports.css = css;
-exports.dev = dev
+exports.versionWebp = versionWebp;
+exports.dev = parallel(versionWebp, dev);
